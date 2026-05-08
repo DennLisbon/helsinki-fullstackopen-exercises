dcc0b2a85887d5ee8d0470f29f9ff72b7f3f5f4c
DennLis <DennisLisboa5@gmail.com>
2026-05-08 13:10:46 -0300

    git push

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,77 @@
+  const Header = () =>{
+
+  const course = 'Half Stack application development'
+
+  return(
+    <h1>{course}</h1>
+  )
+  }
+
+  const Part1 = () =>{
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+
+    return(
+      <p>
+        {part1} {exercises1}
+      </p>
+    )
+  }
+
+  const Content = () =>{
+
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return(
+    <>
+      <Part1 name={part1} exercises={exercises1} />
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </>
+  )
+  }
+  
+  const Total = () =>{
+
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+
+  return(
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+  }
+
+  const App = () => {
+
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
